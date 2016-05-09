dcbaab434ccd68b1249d0efab0b8e065b8dda801
wescraig <wescraig93@gmail.com>
2016-05-09 09:02:42 -0500

    Third commit. Routing functionality added.

--- controllers.js
+++ controllers.js
@@ -1,26 +1,180 @@
-angular.module('waitApp', [])
+angular.module('waitApp', ['ngRoute'])
+.service('calculateMyEarnings', function(){
 
-.controller('waitStaffCtrl', function(){
+	//My Earnings Info
+	var tipTotal = 0;
+	var mealCount = 0;
+	var avgTipPerMeal = 0;
+
+	var subtotal = 0;
+	var tip = 0;
+	var total = 0;
+
+	//Reset Flag
+	var reset = false;
+
+	var setToZero = function(){
+		subtotal = 0;
+		tip = 0;
+		total = 0;
+	}
+
+	var setCustomerCharges = function(sub, tp){
+		subtotal += sub;
+		tip += tp;
+		total = subtotal + tip;
+	}
+
+	var getCustomerCharges = function(){
+		return {
+			"subtotal" : subtotal,
+			"tip" : tip,
+			"total" : total
+		}
+	}
+
+	var addTipToTotal = function(tipAmt){
+		tipTotal+=tipAmt
+	}
+
+	var addToMealCount = function(){
+		mealCount += 1
+	}
+
+	var calcAvgTip = function(){
+		avgTipPerMeal = tipTotal / mealCount;		
+	}
+
+
+	var cancel = function(){
+		tipTotal = 0;
+		mealCount = 0;
+		avgTipPerMeal = 0;
+	}
+
+	var getTipTotal = function(){
+		return tipTotal;
+	}
+
+	var getMealCount = function(){
+		return mealCount;
+	}
+
+	var getAvgTip = function(){
+		return avgTipPerMeal;
+	}
+
+	var setReset = function(boolean){
+		reset = boolean;
+	}
+
+	var getReset = function(){
+		return reset;
+	}
+
+	return {
+		addTipToTotal : addTipToTotal,
+		addToMealCount : addToMealCount,
+		getAvgTip : getAvgTip,
+		getTipTotal : getTipTotal,
+		getMealCount : getMealCount,
+		calcAvgTip: calcAvgTip,
+		cancel : cancel,
+		setReset : setReset,
+		getReset : getReset,
+		setCustomerCharges : setCustomerCharges,
+		getCustomerCharges : getCustomerCharges,
+		setToZero : setToZero
+	};
+
+})
+.config(['$routeProvider', function($routeProvider){
+	$routeProvider.when('/', {
+		templateUrl : 'home.html'
+	})
+	.when('/new_meal', {
+		templateUrl : 'newMeal.html',
+		controller : 'newMealCtrl as vm'
+	})
+	.when('/my_earnings', {
+		templateUrl : 'myEarnings.html',
+		controller : 'myEarningsCtrl as vm'
+	})
+	.otherwise('/');
+}])
+
+.controller('myEarningsCtrl', function(calculateMyEarnings){
+
+	this.tipTotal = calculateMyEarnings.getTipTotal();
+	this.mealCount = calculateMyEarnings.getMealCount();
+	this.avgTipPerMeal = calculateMyEarnings.getAvgTip();
+
+	this.reset = function(form){
+		if(confirm('Are you sure you want to reset your form?')){
+				this.tipTotal = 0;
+				this.mealCount = 0;
+				this.avgTipPerMeal = 0;
+				calculateMyEarnings.setReset(true);
+		}else{
+			return;
+		}
+	}
+
+})
+.controller('newMealCtrl', function(calculateMyEarnings){
+
+	var vm = this;
+
+
+
 	//Meal details
 	this.basePrice;
 	this.taxRate;
 	this.tipPercent;
 
 	//Customer Charges
-	this.subtotal = 0;
-	this.tip = 0;
-	this.total = 0;
-
-	//My Earnings Info
-	this.tipTotal = 0;
-	this.mealCount = 0;
-	this.avgTipPerMeal = 0;
+	this.subtotal = calculateMyEarnings.getCustomerCharges()["subtotal"];
+	this.tip = calculateMyEarnings.getCustomerCharges()["tip"];
+	this.total = calculateMyEarnings.getCustomerCharges()["total"];
 
 	this.cancel = function(form){
 		this.basePrice = "";
 		this.taxRate = "";
 		this.tipPercent = "";
+
+		calculateMyEarnings.setToZero();
+		this.subtotal = calculateMyEarnings.getCustomerCharges()["subtotal"];
+		this.tip = calculateMyEarnings.getCustomerCharges()["tip"];
+		this.total = calculateMyEarnings.getCustomerCharges()["total"];
+
+		calculateMyEarnings.cancel();
+
+		if(form){
+			form.$setPristine();	
+		}
+		
+	}
+
+	if(calculateMyEarnings.getReset()){
+		this.cancel();
+		calculateMyEarnings.setReset(false);
+		return;
+	}
+
+	var clear = function(form){
+		vm.basePrice = "";
+		vm.taxRate = "";
+		vm.tipPercent = "";
 		form.$setPristine();
+	}
+
+
+	var calcTaxes = function(base, taxRate){
+		return base * taxRate / 100;
+	}
+
+	var calcTip = function(base, tipPerc){
+		return base * tipPerc / 100
 	}
 
 	this.submitMeal = function(form){
@@ -28,39 +182,27 @@ angular.module('waitApp', [])
 			form.$setSubmitted(true);
 		}else{
 
-			var base = this.basePrice;
-			var tax = base * this.taxRate / 100;
-			var tip = base * this.tipPercent / 100;
 
-			this.subtotal = base + tax;
-			this.tip = tip;
-			this.total = this.subtotal + this.tip;
+			calculateMyEarnings.setCustomerCharges(this.basePrice + calcTaxes(this.basePrice, this.taxRate), calcTip(this.basePrice, this.tipPercent));
+			// this.subtotal = this.basePrice + calcTaxes(this.basePrice, this.taxRate);
+			// this.tip = calcTip(this.basePrice, this.tipPercent);
+			// this.total = this.subtotal + this.tip;
 
-			this.tipTotal += tip;
-			this.mealCount += 1;
-			this.avgTipPerMeal = this.tipTotal / this.mealCount;
+			this.subtotal = calculateMyEarnings.getCustomerCharges()["subtotal"];
+			this.tip = calculateMyEarnings.getCustomerCharges()["tip"];
+			this.total = calculateMyEarnings.getCustomerCharges()["total"];
 
-			this.cancel(form);
-			
-		}
+			calculateMyEarnings.addTipToTotal(this.tip);
+			calculateMyEarnings.addToMealCount();
+			calculateMyEarnings.calcAvgTip();
 
-
+			clear(form);	
+			calculateMyEarnings.setToZero();
 		
+		}
+	
 	}
 
-	this.reset = function(form){
-		if(confirm('Are you sure you want to reset your form?')){
-				this.cancel(form);
-				this.subtotal = 0;
-				this.tip = 0;
-				this.total = 0;
-				this.tipTotal = 0;
-				this.mealCount = 0;
-				this.avgTipPerMeal = 0;
-		}else{
-			return;
-		}
-	}
 
 	this.interacted = function(form, field){
 		return form.$submitted && !field.$valid;
